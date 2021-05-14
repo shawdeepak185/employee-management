8ffc93044465da047b1ccd2bc4a2f699cf315ac3
shawdeepak185 <shawdeepak185@gmail.com>
2021-05-14 22:12:01 +0530

    check
    error checking

--- src/main/java/com/cg/employeemanagement/controllers/EmployeeController.java
+++ src/main/java/com/cg/employeemanagement/controllers/EmployeeController.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.employeemanagement.entities.Employee;
 import com.cg.employeemanagement.services.IEmployeeServices;
 
-@RestController("/employee")
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
 	
 	@Autowired
